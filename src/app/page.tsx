2cdda628def0c416fc73bf39094494662f812602
Frank Smith <fasvi6@gmail.com>
2023-12-08 22:10:13 -0500

    using reactstrap nowwwww

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,33 @@
 "use client"
 
 import { useCallback, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Button } from 'reactstrap';
 
 export default function Home() {
 
     const [randomCat, setRandomCat] = useState("");
 
     const fetchRandomCat = useCallback(async () => {
-        console.log("helloooooo");
         let response = await fetch("https://api.thecatapi.com/v1/images/search");
         let data = await response.json();
         setRandomCat(data[0].url)
     }, []);
  
     return (
-        <main>
-            <button onClick={fetchRandomCat}>
-                Random cat!
-            </button>
-            {randomCat ? <img src={randomCat} alt="kiggy"/> : <></>}
-        </main>
+        <Container>
+            <Row>
+                <Col>
+                    <Button color="primary" onClick={fetchRandomCat}>
+                        Random cat!
+                    </Button>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    {randomCat ? <img src={randomCat} alt="kiggy"/> : <></>}
+                </Col>
+            </Row>
+        </Container>
     )
 }
