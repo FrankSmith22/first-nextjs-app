f7e26d3eab5f853bf6fc6b69b31384dfe33e65e3
Frank Smith <fasvi6@gmail.com>
2023-12-10 13:16:48 -0500

    sweeet I can now fetch the image and then convert it to binary and write to disk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,6 @@ import Greet from "./greet";
 import RandomCat from "./randomCat";
 
 export default function Home() {
-
-
-    // const fetchRandomCat = useCallback(async () => {
-    //     let response = await fetch("https://api.thecatapi.com/v1/images/search");
-    //     let data = await response.json();
-    //     setRandomCat(data[0].url)
-    // }, []);
  
     return (
         <Container>
--- src/app/randomCat.tsx
+++ src/app/randomCat.tsx
@@ -1,20 +1,32 @@
 "use client"
 
 import { useCallback, useState } from "react";
-import { Response, fetch } from '@tauri-apps/api/http';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Response, fetch, ResponseType } from '@tauri-apps/api/http';
+import { writeBinaryFile, BaseDirectory } from '@tauri-apps/api/fs';
+import { Row, Col, Button } from 'reactstrap';
 
 export default function RandomCat() {
-    const [randomCat, setRandomCat] = useState("");
+    const [randomCatUrl, setRandomCatUrl] = useState("");
+
+    const writeToFile = useCallback(async () => {
+        const image = await fetch(randomCatUrl, {
+            method: "GET",
+            timeout: 30,
+            responseType: ResponseType.Binary
+        })
+        const imageArray = await image.data
+        const fileName = randomCatUrl.slice(randomCatUrl.search(/(\w+)(\.\w+)+(?!.*(\w+)(\.\w+)+)/))
+        await writeBinaryFile(fileName, imageArray, { dir: BaseDirectory.Desktop })
+    }, [randomCatUrl])
 
     const fetchRandomCat = useCallback(async () => {
         const response: Response<String> = await fetch("https://api.thecatapi.com/v1/images/search", {
             method: "GET",
             timeout: 30,
         });
-        setRandomCat(`${response.data[0].url}`)
-    }, [])
- 
+        setRandomCatUrl(`${response.data[0].url}`)
+    }, [randomCatUrl])
+
     return (
         <>
             <Row>
@@ -24,11 +36,17 @@ export default function RandomCat() {
                     </Button>
                 </Col>
             </Row>
-            <Row>
-                <Col>
-                    {randomCat ? <img src={randomCat} alt="kiggy"/> : <></>}
-                </Col>
-            </Row>
+            {randomCatUrl ? <>
+                <Row>
+                    <Col>
+                        <Button color="success" onClick={writeToFile}>Write image to local disk!</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <img src={randomCatUrl} alt="kiggy" />
+                    </Col>
+                </Row></> : <></>}
         </>
     )
 }
